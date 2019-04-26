fd6f749b333f51bd5629d0c5b9e6e535a854aa92
szincone <sawyerzincone@gmail.com>
2019-04-26 11:50:36 -0600

    parsing cmd args off of params

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,7 +7,16 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  char *dir_name;
+  // no argument recieved
+  if (argc == 1)
+  {
+    dir_name = ".";
+  }
+  else if (argc == 2)
+  {
+    dir_name = argv[1];
+  }
   // Open directory
 
   // Repeatly read and print entries
